029f103a3cfce43e8a66f3c7089d24bd2ffc9819
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2023-02-16 12:01:48 +0530

    Stay Now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,7 +42,7 @@ const NavBar = () => {
               noWrap
               sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
             >
-              You Are Welcome
+              Place To Stay
             </Typography>
             <Typography
               variant="h6"
@@ -50,7 +50,7 @@ const NavBar = () => {
               noWrap
               sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
             >
-              YRW
+              PTS
             </Typography>
             {!currentUser ? (
               <Button
