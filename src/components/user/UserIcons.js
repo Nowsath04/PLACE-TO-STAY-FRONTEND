12eff2fba7d0c80e5c536089d60131f10cf8d962
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2023-02-24 14:47:22 +0530

    icon

--- src/components/user/UserIcons.js
+++ src/components/user/UserIcons.js
@@ -1,9 +1,9 @@
-import { Mail, Notifications } from '@mui/icons-material';
-import { Avatar, Badge, Box, IconButton, Tooltip } from '@mui/material';
-import React, { useState } from 'react';
-import { useValue } from '../../context/ContextProvider';
-import useCheckToken from '../../hooks/useCheckToken';
-import UserMenu from './UserMenu';
+import { Mail, Notifications } from "@mui/icons-material";
+import { Avatar, Badge, Box, IconButton, Tooltip } from "@mui/material";
+import React, { useState } from "react";
+import { useValue } from "../../context/ContextProvider";
+import useCheckToken from "../../hooks/useCheckToken";
+import UserMenu from "./UserMenu";
 
 const UserIcons = () => {
   useCheckToken();
@@ -15,16 +15,6 @@ const UserIcons = () => {
 
   return (
     <Box>
-      <IconButton size="large" color="inherit">
-        <Badge color="error" badgeContent={5}>
-          <Mail />
-        </Badge>
-      </IconButton>
-      <IconButton size="large" color="inherit">
-        <Badge color="error" badgeContent={20}>
-          <Notifications />
-        </Badge>
-      </IconButton>
       <Tooltip title="Open User Settings">
         <IconButton onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
           <Avatar src={currentUser?.photoURL} alt={currentUser?.name}>
